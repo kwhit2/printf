958f1754b64022dc5830858bda079ac8c34c4261
sang-nguy0920 <2321@holbertonschool.com>
2020-10-25 22:56:07 +0000

    Day 3, No signs of Life

---
+++ format_specifier.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+
+/**
+ *print_all -  function that prints anything.
+ *@format: *format const pointer
+ *
+ *Description:  function that prints anything.
+ *section header: Section description
+ *Return: returns void
+ */
+
+int (*get_format_func(char *s))(va_list)
+{
+print_t print[] = {
+{"c", print_char},
+{"s", print_string},
+{NULL, NULL}
+};
+
+unsigned int i;
+
+for (i = 0; print[i].f != NULL; i++)
+{
+if (*(print[i]).prt == s[0])
+return (print[i].f);
+}
+return (NULL);
+}
