dfaaf3aab7e30290dec5d16a773b1caf87131a9a
sang-nguy0920 <2321@holbertonschool.com>
2020-10-27 16:31:08 +0000

    Created backup dir

---
+++ backup/holberton.h
@@ -0,0 +1,28 @@
+#ifndef _PRINTF_FUNCTION
+#define _PRINTF_FUNCTION
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * struct print - c, s, i, d.
+ * @prt: *ptr point
+ * @f: pointer to va_list
+ *
+ * Description: va_list struct
+ */
+typedef struct print
+{
+char *prt;
+int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int print_char(va_list a);
+int print_string(va_list b);
+
+int _printf(const char *format, ...);
+
+
+#endif
